4678005873539929ebc295d5525ce196d6dfd20c
TatyanaGuntik <tstepchina@gmail.com>
2021-03-07 18:48:10 +0200

    init

---
+++ index.php
@@ -0,0 +1,420 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Title</title>
+    <link href="style.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+</head>
+<body>
+
+<header id="header">
+    <div class="wrapper">
+        <nav>
+            <img src="media/Logo.png" class="logo" alt="">
+
+            <div id="burger">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+            <div class="navigation-links">
+                <a href="">Home</a>
+                <a href="">About us</a>
+                <a href="">Portfolio</a>
+                <a href="">Pricing</a>
+                <a href="">Team</a>
+                <a href="">Blog</a>
+                <a href="">Contact</a>
+            </div>
+        </nav>
+        <div class="navigation-hide" id="navigation-hide">
+            <a href="">Home</a>
+            <a href="">About us</a>
+            <a href="">Portfolio</a>
+            <a href="">Pricing</a>
+            <a href="">Team</a>
+            <a href="">Blog</a>
+            <a href="">Contact</a>
+        </div>
+        <div class="single-item">
+            <div class="welcome-block">
+                <div class="welcome-pre">Our Clients Are Our First Priority</div>
+                <div class="welcome-title">WELCOME TO BINO</div>
+                <div class="separator">
+                    <div class="line"></div>
+                    <div class="point"></div>
+                    <div class="line"></div>
+                </div>
+                <div class="welcome-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem
+                    Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+                    a
+                    galley of type and scrambled it to make a type specimen book.
+                </div>
+                <div class="welcome-buttons">
+                    <a href="" class="welcome-learn-more">Learn more</a>
+                    <a href="" class="welcome-get-started">Get started now</a>
+                </div>
+            </div>
+            <div class="welcome-block">
+                <div class="welcome-pre">Our Clients Are Our First Priority</div>
+                <div class="welcome-title">WELCOME TO BINO</div>
+                <div class="separator">
+                    <div class="line"></div>
+                    <div class="point"></div>
+                    <div class="line"></div>
+                </div>
+                <div class="welcome-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem
+                    Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+                    a
+                    galley of type and scrambled it to make a type specimen book.
+                </div>
+                <div class="welcome-buttons">
+                    <a href="" class="welcome-learn-more">Learn more</a>
+                    <a href="" class="welcome-get-started">Get started now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<main>
+    <section class="our-blog">
+        <div class="our-blog-header">
+            <div class="our-blog-title">OUR BLOG</div>
+            <p class="our-blog-text">Suspendisse sed eros mollis, tincidunt felis eget, interdum era</p>
+            <div class="separator">
+                <div class="line"></div>
+                <div class="point"></div>
+                <div class="line"></div>
+            </div>
+        </div>
+        <div class="our-blog-items">
+            <img src="media/coffee.png" alt="">
+            <div class="t-shirt-design toLeft">
+                <div class="t-shirt-design_wrapper">
+                    <div class="t-shirt-pre">art/t-shirt</div>
+                    <div class="t-shirt-title">T-shirt design</div>
+                    <div class="t-shirt-title"></div>
+                    <div class="t-shirt-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem
+                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+                        took
+                        a
+                        galley of type and scrambled it to make a type specimen book.
+                    </div>
+                    <div class="t-shirt-readmore"><a href="">Read more >></a></div>
+                </div>
+            </div>
+            <img src="media/clock.png" alt="">
+            <div class="t-shirt-design toLeft">
+                <div class="t-shirt-design_wrapper">
+                    <div class="t-shirt-pre">art/t-shirt</div>
+                    <div class="t-shirt-title">T-shirt design</div>
+                    <div class="t-shirt-title"></div>
+                    <div class="t-shirt-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem
+                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+                        took
+                        a
+                        galley of type and scrambled it to make a type specimen book.
+                    </div>
+                    <div class="t-shirt-readmore"><a href="">Read more >></a></div>
+                </div>
+            </div>
+            <div class="t-shirt-design toRight">
+                <div class="t-shirt-design_wrapper">
+                    <div class="t-shirt-pre">art/t-shirt</div>
+                    <div class="t-shirt-title">T-shirt design</div>
+                    <div class="t-shirt-title"></div>
+                    <div class="t-shirt-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem
+                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+                        took
+                        a
+                        galley of type and scrambled it to make a type specimen book.
+                    </div>
+                    <div class="t-shirt-readmore"><a href="">Read more >></a></div>
+                </div>
+            </div>
+            <img src="media/road.png" alt="">
+            <div class="t-shirt-design toRight">
+                <div class="t-shirt-design_wrapper">
+                    <div class="t-shirt-pre">art/t-shirt</div>
+                    <div class="t-shirt-title">T-shirt design</div>
+                    <div class="t-shirt-title"></div>
+                    <div class="t-shirt-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem
+                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+                        took
+                        a
+                        galley of type and scrambled it to make a type specimen book.
+                    </div>
+                    <div class="t-shirt-readmore"><a href="">Read more >></a></div>
+                </div>
+            </div>
+            <img src="media/car.png" alt="">
+        </div>
+        <div class="our-blog-icons">
+            <div class="wrapper">
+                <div class="our-blog-icon">
+                    <img src="media/Icon1.png" alt="">
+                    <p class="our-blog-icon_number">3891</p>
+                    <p class="our-blog-icon_text">User Favourites</p>
+                </div>
+                <div class="our-blog-icon">
+                    <img src="media/Icon2.png" alt="">
+                    <p class="our-blog-icon_number">281K</p>
+                    <p class="our-blog-icon_text">Posts last 24 hours</p>
+                </div>
+                <div class="our-blog-icon">
+                    <img src="media/Icon3.png" alt="">
+                    <p class="our-blog-icon_number">3891</p>
+                    <p class="our-blog-icon_text">Posts last 24 hours</p>
+                </div>
+                <div class="our-blog-icon">
+                    <img src="media/Icon4.png" alt="">
+                    <p class="our-blog-icon_number">3891</p>
+                    <p class="our-blog-icon_text">Posts last 24 hours</p>
+                </div>
+                <div class="our-blog-icon">
+                    <img src="media/Icon5.png" alt="">
+                    <p class="our-blog-icon_number">3891</p>
+                    <p class="our-blog-icon_text">Posts last 24 hours</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="our-pricing">
+        <div class="our-pricing-header">
+            <div class="our-pricing-title">OUR PRICING</div>
+            <p class="our-pricing-text">A 30 days free trial for all. A brief story about how this proc</p>
+            <div class="separator">
+                <div class="line"></div>
+                <div class="point"></div>
+                <div class="line"></div>
+            </div>
+        </div>
+        <div class="our-pricing-items wrapper">
+
+
+            <div class="our-pricing-item_gray">
+                <div class="our-pricing-item_header">Starter</div>
+                <div class="our-pricing-item_price">
+                    <div class="sum">$19</div>
+                    <p>per month</p>
+                </div>
+                <p>Competition Analysis Method</p>
+                <p>All Ranked URLs</p>
+                <p>International Support System</p>
+                <p>Social Media Tracing</p>
+                <div class="our-pricing-item_buttom"><a href="">Choose plan</a></div>
+            </div>
+            <div class="our-pricing-item_red">
+                <div class="our-pricing-item_header">Starter</div>
+                <div class="our-pricing-item_price">
+                    <div class="sum">$19</div>
+                    <p>per month</p>
+                </div>
+                <p>Competition Analysis Method</p>
+                <p>All Ranked URLs</p>
+                <p>International Support System</p>
+                <p>Social Media Tracing</p>
+                <div class="our-pricing-item_buttom"><a href="">Choose plan</a></div>
+            </div>
+            <div class="our-pricing-item_gray">
+                <div class="our-pricing-item_header">Starter</div>
+                <div class="our-pricing-item_price">
+                    <div class="sum">$19</div>
+                    <p>per month</p>
+                </div>
+                <p>Competition Analysis Method</p>
+                <p>All Ranked URLs</p>
+                <p>International Support System</p>
+                <p>Social Media Tracing</p>
+                <div class="our-pricing-item_buttom"><a href="">Choose plan</a></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="our-team">
+        <div class="our-team-header">
+            <div class="our-team-title">OUR TEAM</div>
+            <p class="our-team-text">Meet the craziest team. Share your thoughts with them.</p>
+            <div class="separator">
+                <div class="line"></div>
+                <div class="point"></div>
+                <div class="line"></div>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="our-team-item1">
+                <img src="media/team1.png" class="team-img" alt="">
+                <div class="team-info">
+                    <div class="our-team_vertical-line"></div>
+                    <p class="name">Kazi Erfan</p>
+                    <p class="position">UI/UX Designer</p>
+                </div>
+            </div>
+            <div class="our-team-item2">
+                <img src="media/team1.png" class="team-img" alt="">
+                <div class="team-info">
+                    <div class="our-team_vertical-line"></div>
+                    <p class="name">Kazi Erfan</p>
+                    <p class="position">UI/UX Designer</p>
+                </div>
+            </div>
+            <div class="our-team-item3">
+                <img src="media/team1.png" class="team-img" alt="">
+                <div class="team-info">
+                    <div class="our-team_vertical-line"></div>
+                    <p class="name">Kazi Erfan</p>
+                    <p class="position">UI/UX Designer</p>
+                </div>
+            </div>
+            <div class="our-team-item4">
+                <img src="media/team1.png" class="team-img" alt="">
+                <div class="team-info">
+                    <div class="our-team_vertical-line"></div>
+                    <p class="name">Kazi Erfan</p>
+                    <p class="position">UI/UX Designer</p>
+                </div>
+            </div>
+            <div class="our-team-item5">
+                <img src="media/team1.png" class="team-img" alt="">
+                <div class="team-info">
+                    <div class="our-team_vertical-line"></div>
+                    <p class="name">Kazi Erfan</p>
+                    <p class="position">UI/UX Designer</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="keep-intouch">
+        <div class="keep-intouch-header">
+            <div class="keep-intouch-title">kEEP IN TOUCH</div>
+            <p class="keep-intouch-text">Nullam sit amet odio eu est aliquet euismod a a urna. Proin eu</p>
+            <div class="separator">
+                <div class="line"></div>
+                <div class="point"></div>
+                <div class="line"></div>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="contacts">
+                <div class="address">
+                    <div class="contacts-title">Our Address</div>
+                    <p>House #13, Streat road, Sydney 2310 Australia</p>
+                </div>
+                <div class="call-us">
+                    <div class="contacts-title">Call us</div>
+                    <a href="tel:+8801681091425">+ 880 168 109 1425</a><br>
+                    <a href="tel:+0216809142">+ 0216809142</a>
+                </div>
+                <div class="mail-us">
+                    <div class="contacts-title">Mail us</div>
+                    <a href="mailto:contactus@email.com">contactus@email.com</a>
+                </div>
+            </div>
+            <form id="form" method="POST">
+                <input type="text" placeholder="Name" name="name"/>
+                <input type="text" placeholder="Email" name="email"/>
+                <input type="text" placeholder="Subject" name="subject"/>
+                <textarea id="" cols="30" rows="5" placeholder="Message" name="message"></textarea>
+                <button type="button">Send message</button>
+            </form>
+        </div>
+    </section>
+</main>
+<footer>
+    <a href="#header" class="up-arrow"><img src="media/UpArrow.png" alt=""></a>
+    <div class="wrapper">
+        <div class="social">
+            <a href=""><img src="media/facebook.png" alt=""></a>
+            <a href=""><img src="media/twitter.png" alt=""></a>
+            <a href=""><img src="media/rss.png" alt=""></a>
+            <a href=""><img src="media/googleplus.png" alt=""></a>
+            <a href=""><img src="media/linkedin.png" alt=""></a>
+            <a href=""><img src="media/skype.png" alt=""></a>
+            <a href=""><img src="media/vimeo.png" alt=""></a>
+            <a href=""><img src="media/tumblr.png" alt=""></a>
+        </div>
+        <div class="copywrite">Kazi Erfan © All Rights Reserved</div>
+    </div>
+</footer>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="slick/slick.min.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.single-item').slick();
+    });
+
+    $('#burger').on('click', function () {
+            var condition = $("#navigation-hide a").hasClass("visual");
+            if (!condition) {
+                $('#navigation-hide a').addClass("visual");
+            } else {
+                $('#navigation-hide a').removeClass("visual");
+            }
+        }
+    );
+
+    $('#form button').on('click', function () {
+        var form = $('#form');
+        var error = false;
+        form.find('input, textarea').each(function () {
+            if ($(this).val() == '') {
+                $(this).attr("placeholder", "Fill in the field!");
+                error = true;
+            }
+        });
+
+        if (!error) {
+            var data = form.serialize();
+            $.ajax({
+                type: 'POST',
+                url: 'send.php',
+                dataType: 'json',
+                data: data,
+                beforeSend: function (data) {
+                    form.find('button[type="button"]').attr('disabled', 'disabled');
+                },
+                success: function (data) {
+                    if (data['error']) {
+                        alert(data['error']);
+                    } else {
+                        alert('we received your letter');
+                        form.find('input, textarea').each(function () {
+                            $(this).val("");
+                        });
+                    }
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    alert(xhr.status);
+                    alert(thrownError);
+                },
+                complete: function (data) { // событие после любого исхода
+                    form.find('button[type="button"]').prop('disabled', false);
+                }
+            });
+        }
+    });
+</script>
+
+</body>
+</html>
